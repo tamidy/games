e2e53fb77afea6625afa0a91219c935d31ee60fb
csc470-fall2019 <yousafit@yahoo.com>
2021-04-11 22:21:59 -0400

    made two more html files

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 (function() {
     "use strict";
 
+    //Arrays to hold links to games and sketches
+    let games = [
+        "https://tamidy.github.io/pirate-ship-vs-sea-monster-js/", 
+        "https://tamidy.github.io/rabbit-vs-fox-js/"
+    ];
+    let sketches = [
+        "https://tamidy.github.io/blue/", 
+        "https://tamidy.github.io/sun-and-moon/", 
+        "https://tamidy.github.io/electric-lasers/"
+    ];
+
     window.onload = function() {
-        
+
     }
 
 }) ();
